d9465da4455be00d67f88afbbd7f3d86b0ca1f76
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-08-20 18:19:28 +0300

    Add files via upload

---
+++ Python_Advanced/07.comprehensions_lab/03.even_matrix.py
@@ -0,0 +1,9 @@
+def parse_row(string):
+    return map(int, string.split(", "))
+
+
+rows_count = int(input())
+matrix = [parse_row(input()) for _ in range(rows_count)]
+
+result = [[x for x in row if x % 2 == 0] for row in matrix]
+print(result)
