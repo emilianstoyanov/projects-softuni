f436b44612154167e1f8bf7176dd8fddfd69d8a8
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-08-26 08:51:26 +0300

    Add files via upload

---
+++ Python_Advanced/02.lists_as_stacks_and_queues_exercise/05.Truck Tour.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n = int(input())
+pumps = deque()
+
+for _ in range(n):
+    pump = [int(x) for x in input().split()]
+    pumps.append(pump)
+
+for i in range(n):
+    is_valid = True
+    fuel = 0
+
+    for _ in range(n):
+        current = pumps.popleft()
+        fuel += current[0] - current[1]
+        if fuel < 0:
+            is_valid = False
+        pumps.append(current)
+
+    if is_valid:
+        print(i)
+        break
+    pumps.append(pumps.popleft())
+
+
+
+
+
+
