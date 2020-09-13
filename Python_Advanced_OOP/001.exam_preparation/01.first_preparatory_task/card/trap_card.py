88d51a8ea542238275754e0bbde8d70423e98128
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-09-13 09:04:45 +0300

    Update trap_card.py

--- Python_Advanced_OOP/001.exam_preparation/01.first_preparatory_task/card/trap_card.py
+++ Python_Advanced_OOP/001.exam_preparation/01.first_preparatory_task/card/trap_card.py
@@ -2,10 +2,5 @@ from project.card.card import Card
 
 
 class TrapCard(Card):
-    def __init__(self, name: str):  # конструктура
+    def __init__(self, name: str):  
         super().__init__(name, damage_points=120, health_points=5)
-
-# TrapCard
-# Has 120 damage points and 5 health points.
-# Constructor should take the following values upon initialization:
-# name
