8a1784846931be56af3e509a4852ec1229abe25f
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-09-13 09:03:55 +0300

    Update magic_card.py

--- Python_Advanced_OOP/001.exam_preparation/01.first_preparatory_task/card/magic_card.py
+++ Python_Advanced_OOP/001.exam_preparation/01.first_preparatory_task/card/magic_card.py
@@ -2,10 +2,5 @@ from project.card.card import Card
 
 
 class MagicCard(Card):
-    def __init__(self, name: str):  # конструктура
+    def __init__(self, name: str):  
         super().__init__(name, damage_points=5, health_points=80)
-
-# MagicCard
-# Has 5 damage points and 80 health points.
-# Constructor should take the following values upon initialization:
-# name
