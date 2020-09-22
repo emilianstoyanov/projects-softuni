9ebbdd53fb51dcb97a16fe9247871e9dd521e53b
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-09-23 00:05:20 +0300

    Update 02.password.py

--- Python_Fundamentals/a_exam_preparation/02.password.py
+++ Python_Fundamentals/a_exam_preparation/02.password.py
@@ -12,8 +12,8 @@ for i in range(1, count + 1):
 
 
     if left[0] == right[1] and len(left[0]) == len(right[1]):
-        if ">" in separation[0] and "<" in separation[-1]:  # проверка за знаците, дали се съдържат в началото и в края
-            if text.count("|") == 3:  # Проверка, дали съдържа 3 броя чертички
+        if ">" in separation[0] and "<" in separation[-1]: 
+            if text.count("|") == 3: 
                 if left[1].isdigit() and separation[1].islower() and separation[2].isupper():
                     serch = right[0].count("<")
                     serch2 = right[0].count(">")
@@ -34,21 +34,3 @@ for i in range(1, count + 1):
             print("Try another password!")
     else:
         print("Try another password!")
-
-
-
-
-
-# Започва с група символи и завършва със същите символи (една и съща дължина)
-
-# Има повече от знак (>) след първата група и по-малко от знак (<) преди последната
-
-# Между по-големи от знака и по-малко от знак има четири групи (всяка от три знака),
-# разделени с тръба ("|")
-
-# - пътвата група са само числа
-# втората група са малки букви
-# третата група са големи букви
-# четвъртата само символи
-
-# При невалидна парола, принт: "Try another password!"
