eeac4fab53acbfa060dda7a2f29007315682aab4
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-09-27 18:08:42 +0300

    Add files via upload

---
+++ Python_Fundamentals/dictionaries_lab/04.оdd_оccurrences.py
@@ -0,0 +1,39 @@
+words = input().split(" ")
+dictionary = {}
+
+for word in words:
+    word_lower = word.lower()
+    if word_lower not in dictionary:
+        dictionary[word_lower] = 0
+    dictionary[word_lower] += 1
+
+for (key, value) in dictionary.items():
+    if value % 2 != 0:
+        print(key, end=" ")
+
+
+
+
+
+
+
+
+
+
+# def solve(words_str):
+#     words = [word.lower() for word in words_str.split(" ")]
+#     words_occurrences = {}
+#     for word in words:
+#         if word not in words_occurrences:
+#             words_occurrences[word] = 0
+#         words_occurrences[word] += 1
+#
+#     odd_words = [word for (word, count)
+#                  in words_occurrences.items()
+#                  if count % 2 == 1]
+#
+#     print(" ".join(odd_words))
+#
+# tests = [input()]
+#
+# [solve(test) for test in tests]
