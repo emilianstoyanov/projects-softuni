e5bae790669946d9fdeabb8ed02f278adba71496
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-09-29 00:55:53 +0300

    Add files via upload

---
+++ Python_Fundamentals/dictionaries _exercise/04.Orders.py
@@ -0,0 +1,21 @@
+quantities_dict = {}
+prices_dict = {}
+while True:
+    input_line = input()
+
+    if input_line == "buy":
+        break
+
+    (product, price_str, quantity_str) = input_line.split(" ")  # quantity = input_line[0], float(input_line[1], int(input_line[2])
+    price = float(price_str)  # друг начин за вход
+    quantity = int(quantity_str)
+
+    prices_dict[product] = price
+
+    if product not in quantities_dict:
+        quantities_dict[product] = 0
+    quantities_dict[product] += quantity
+
+for (key, price) in prices_dict.items():
+    total_sum = price * quantities_dict[key]
+    print(f"{key} -> {total_sum:.2f}")
