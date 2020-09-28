dd82e7a5f1b6a5e593f37255063199fbd685f2d1
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-09-29 00:58:33 +0300

    Add files via upload

---
+++ Python_Fundamentals/dictionaries _exercise/06.Courses.py
@@ -0,0 +1,58 @@
+reg = {}
+
+while True:
+    text = input().split(" : ")
+    if text[0] == "end":
+        break
+
+    course = text[0]
+    name = text[1]
+
+    if course not in reg:
+        reg[course] = []
+    reg[course].append(name)
+
+sorted_dict = dict(sorted(reg.items(), key=lambda x: (-len(x[1]))))
+
+for (key, value) in sorted_dict.items():
+    if len(value) > 0:
+        print(f"{key}: {len(value)}")
+        [print(f"-- {name}") for name in sorted(value)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# reg = {}
+#
+# while True:
+#     text = input().split(" : ")
+#     if text[0] == "end":
+#         break
+#
+#     course = text[0]
+#     name = text[1]
+#
+#     if course not in reg:
+#         reg[course] = []
+#     reg[course].append(name)
+#
+# sorted_dict = dict(sorted(reg.items(), key=lambda x: (-len(x[1]))))
+#
+# for (key, value) in sorted_dict.items():
+#     if len(value) > 0:
+#         print(f"{key}: {len(value)}")
+#         [print(f"-- {name}") for name in sorted(value)]
+
+# [print(f"! {user}") for user in sorted(value)]
