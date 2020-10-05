e7456496fc51e7900886fcf91d5d542bbf7d4ddb
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-10-06 00:10:21 +0300

    Create 04.orders.py

---
+++ Python_Fundamentals/functions_lab/04.orders.py
@@ -0,0 +1,25 @@
+def find_price_per_unit(product):
+    if product == "coffee":
+        return 1.5
+
+    if product == "water":
+        return 1.0
+
+    if product == "coke":
+        return 1.4
+
+    if product == "snacks":
+        return 2.0
+
+
+def get_order_total(product, quantity):
+    price_per_unit = find_price_per_unit(product)
+    if price_per_unit is None:
+        return 0.0
+    return price_per_unit * quantity
+
+
+product = input()
+quantity = int(input())
+
+print(f"{get_order_total(product, quantity):.2f}")
