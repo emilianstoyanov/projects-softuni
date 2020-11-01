2a4441077de4fe68797f8626272c44d6bdbd7d09
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-11-01 08:27:03 +0200

    Create 02.person_info.py

---
+++ Python_Advanced/09.functions_advanced_lab/02.person_info.py
@@ -0,0 +1,8 @@
+def get_info(name, town, age):
+    return f"This is {name} from {town} and he is {age} years old"
+
+
+
+info = {"name": "George", "town": "Sofia", "age": 20}
+
+print(get_info(**info))
