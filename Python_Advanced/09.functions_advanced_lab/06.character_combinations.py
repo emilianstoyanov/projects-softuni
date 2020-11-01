0e85f64b4faa7efdf646f352f28dc5fb774be769
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-11-01 08:27:47 +0200

    Create 06.character_combinations.py

---
+++ Python_Advanced/09.functions_advanced_lab/06.character_combinations.py
@@ -0,0 +1,15 @@
+def print_comb(text, idx):
+    if idx >= len(text):
+        print("".join(text))
+        return
+
+    print_comb(text, idx + 1)
+    for i in range(idx + 1, len(text)):
+        text[idx], text[i] = text[i], text[idx]
+        print_comb(text, idx + 1)
+        text[idx], text[i] = text[i], text[idx]
+
+
+
+text = list(input())
+print_comb(text, 0)
